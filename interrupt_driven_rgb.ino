9e54476fb2ce316e3bb8a6261356570670ffc9d2
TJ Peden <tj.peden@tj-coding.com>
2014-06-07 17:05:55 -0500

    All basic features working.
    Still need to add EEPROM support and look into SD card support

--- interrupt_driven_rgb.ino
+++ interrupt_driven_rgb.ino
@@ -1,14 +1,14 @@
-// #include <avr/sleep.h>
 // #include <pitches.h>
 
 #include <SPI.h>
 #include <Time.h>
 // #include <EEPROM.h>
+#include <Button.h>
 #include <Ucglib.h>
 #include <TinyGPS++.h>
 #include <Streaming.h>
 #include <TimedEvent.h>
-#include <ButtonEvent.h>
+#include <LowPower_Teensy3.h>
 #include <FiniteStateMachine.h>
 
 #define OLED_MOSI 11
@@ -25,58 +25,59 @@
 
 #define arraySize(a) (sizeof(a) / sizeof(a[0]))
 
-const uint8_t buttonPin = 2;
+const uint8_t buttonPin = 6;
 // const uint8_t id_address = 0;
 const double threshold = 0.01;
-// unsigned long start = 0;
 double distanceTo;
 double course;
 uint8_t waypoint_index = 0;
 
 const double waypoint[][2] = {
-  { 36.139670, -96.004777 },
-  { 36.139777, -96.003538 },
-  { 36.140135, -96.004070 },
+  { 36.049521, -95.921004 },
+  { 36.048774, -95.921697 },
+  // { 36.140135, -96.004070 },
 };
+
+Button button = Button(buttonPin);
 
 // U8GLIB_SSD1351_128X128_HICOLOR oled(OLED_CS, OLED_DC, OLED_RESET);
 Ucglib_SSD1351_18x128x128_HWSPI oled(OLED_DC, OLED_CS, OLED_RESET);
 
+TEENSY3_LP LP = TEENSY3_LP();
+
 TinyGPSPlus gps;
 
-// TinyGPSCustom ack_command(gps, "PMTK001", 1);
-// TinyGPSCustom ack_response(gps, "PMTK001", 2);
-// TinyGPSCustom awake(gps, "PMTK010", 1);
+TinyGPSCustom ack_command(gps, "PMTK001", 1);
+TinyGPSCustom ack_response(gps, "PMTK001", 2);
+TinyGPSCustom awake(gps, "PMTK010", 1);
 
 State GetFix       = State(enterGetFix, updateGetFix, exitGetFix);
 State ReadLocation = State(enterReadLocation, updateReadLocation, exitReadLocation);
 State SleepModules = State(enterSleepModules, updateSleepModules, NULL);
 State SleepNow     = State(updateSleepNow);
 State WakeModules  = State(enterWakeModules, updateWakeModules, NULL);
-State NextWaypoint = State(enterNextWaypoint, updateNextWaypoint, NULL);
-State LastWaypoint = State(updateLastWaypoint);
+State NextWaypoint = State(enterNextWaypoint, NULL, NULL);
+State LastWaypoint = State(enterLastWaypoint, NULL, NULL);
 State GPSFailure   = State(enterGPSFailure, NULL, NULL);
 
 FSM stateMachine = FSM(GetFix);
 
-// void pictureLoop(void (draw)()) {
-//   oled.firstPage();
-//   do {
-//     draw();
-//   } while(oled.nextPage());
-// }
+void onClick(Button& button) {
+  if(stateMachine.isInState(NextWaypoint)) {
+    if(waypoint_index >= arraySize(waypoint)) {
+      stateMachine.transitionTo(LastWaypoint);
+    } else {
+      stateMachine.transitionTo(ReadLocation);
+    }
+  }
+}
 
-void onDown(ButtonInformation* sender) { /* do nothing here */ }
-
-void onUp(ButtonInformation* sender) { /* do nothing here */ }
-
-void onDouble(ButtonInformation* sender) { /* do nothing here */ }
-
-void onHold(ButtonInformation* sender) {
-  if(millis() < 6000) {
-    // reset waypoint index
+void onHold(Button& button) {
+  if(millis() < 6000 || stateMachine.isInState(LastWaypoint)) {
+    waypoint_index = 0;
   } else {
-    stateMachine.transitionTo(NextWaypoint);
+    if(gps.location.isValid())
+      stateMachine.transitionTo(NextWaypoint);
   }
 }
 
@@ -111,9 +112,6 @@ void enterGetFix() {
   oled.setColor(255, 255, 0);
   oled.setPrintPos(0, oled.getHeight() / 2);
   oled << F("Please wait...");
-
-  int width = oled.getStrWidth("0 satellites");
-  oled.setClipRange(0, oled.getHeight() - 12, width, 12);
 
   TimedEvent.start(GET_FIX_TIMER);
 }
@@ -186,36 +184,68 @@ void enterSleepModules() {
   oled.setColor(255, 255, 0);
   oled.setPrintPos(0, oled.getHeight() / 2);
   oled << F("Please wait...");
+
+  Serial3.println(F("$PMTK161,0*28")); // Sleep GPS
 }
 
+
 void updateSleepModules() {
+  if(ack_command.isUpdated())
+    if(strcmp(ack_command.value(), "161") + strcmp(ack_response.value(), "3") == 0)
+      stateMachine.transitionTo(SleepNow);
 }
 
 void updateSleepNow() {
+  oled.clearScreen();
+
+  LP.DeepSleep(GPIO_WAKE, PIN_6);
+
+  stateMachine.transitionTo(WakeModules);
 }
 
 void enterWakeModules() {
-  ;
+  drawTitle(F("Waking"));
+  oled.setFont(ucg_font_fixed_v0r);
+
+  oled.setColor(255, 255, 0);
+  oled.setPrintPos(0, oled.getHeight() / 2);
+  oled << F("Please wait...");
+
+  Serial3.println(' ');
 }
 
 void updateWakeModules() {
-  drawWakeModules();
+  if(awake.isUpdated()) {
+    awake.value();
+    delay(1000);
+    stateMachine.transitionTo(GetFix);
+  }
 }
 
 void enterNextWaypoint() {
-  ;
+  drawTitle(F("Waypoint Reached"));
+  oled.setFont(ucg_font_fixed_v0r);
+
+  oled.setColor(255, 255, 0);
+  oled.setPrintPos(0, oled.getHeight() / 2);
+  oled << F("Push the button to");
+  oled.setPrintPos(0, (oled.getHeight() / 2) + (oled.getFontAscent() - oled.getFontDescent()));
+  oled << F("continue.");
+
+  waypoint_index++;
 }
 
-void updateNextWaypoint() {
-  drawNextWaypoint();
-}
-
-void updateLastWaypoint() {
-  drawLastWaypoint();
+void enterLastWaypoint() {
+  drawTitle(F("Open the Box"));
 }
 
 void enterGPSFailure() {
-  drawGPSFailure();
+  drawTitle(F("GPS Failure"));
+  oled.setFont(ucg_font_fixed_v0r);
+
+  oled.setColor(255, 255, 0);
+  oled.setPrintPos(0, oled.getHeight() / 2);
+  oled << F("No data from GPS!");
 }
 
 void drawReadLocation() {
@@ -242,86 +272,27 @@ void drawReadLocation() {
   oled << datetime;
 
   oled.setFont(ucg_font_7x13Br);
+  oled.setColor(0, 255, 0);
 
-  oled.setPrintPos(0, oled.getHeight() / 2 - 20);
+  oled.setPrintPos(0, oled.getHeight() / 2);
   oled << distanceTo << F(" miles");
 
-  oled.setPrintPos(0, oled.getHeight() / 2 + 20);
+  oled.setPrintPos(0, (oled.getHeight() / 2) + (oled.getFontAscent() - oled.getFontDescent()));
   oled << TinyGPSPlus::cardinal(course);
 
   oled.setFont(ucg_font_fixed_v0r);
+  oled.setColor(255, 255, 255);
 
-  oled.setPrintPos(0, oled.getHeight() - (oled.getFontDescent() + 12));
+  oled.setPrintPos(0, oled.getHeight() + oled.getFontDescent());
   oled << status;
 
-  oled.setPrintPos(0, oled.getHeight() - (oled.getFontDescent() + 24));
-  oled << F("hdop: ") << gps.hdop.value() << F(" WP: ") << (waypoint_index + 1) << '/' << arraySize(waypoint);
-}
-
-void drawSleepModules() {
-  oled.clearScreen();
-  oled.setColor(0, 0, 255);
-  oled.drawRFrame(0, 0, oled.getWidth(), 20, 3);
-
-  oled.setFont(ucg_font_7x13Br);
-
-  oled.setColor(255, 255, 255);
-  oled.setPrintPos(5, 15);
-  oled << F("Sleep Modules");
-}
-
-void drawSleepNow() {
-  ;
-}
-
-void drawWakeModules() {
-  ;
-}
-
-void drawNextWaypoint() {
-  oled.clearScreen();
-  oled.setColor(0, 0, 255);
-  oled.drawRFrame(0, 0, oled.getWidth(), 20, 3);
-
-  oled.setFont(ucg_font_7x13Br);
-
-  oled.setColor(255, 255, 255);
-  oled.setPrintPos(5, 15);
-  oled << F("Next Waypoint");
-}
-
-void drawLastWaypoint() {
-  ;
-}
-
-void drawGPSFailure() {
-  oled.clearScreen();
-  oled.setColor(0, 0, 255);
-  oled.drawRFrame(0, 0, oled.getWidth(), 20, 3);
-
-  oled.setFont(ucg_font_7x13Br);
-
-  oled.setColor(255, 255, 255);
-  oled.setPrintPos(5, 15);
-  oled << F("GPS Failure");
-
-  oled.setFont(ucg_font_fixed_v0r);
-
-  oled.setColor(255, 255, 0);
-  oled.setPrintPos(0, oled.getHeight() / 2);
-  oled << F("No data from GPS!");
+  oled.setPrintPos(0, oled.getHeight() + (oled.getFontDescent() + 12));
+  oled << F("hdop: ") << gps.hdop.value() << F(" WP: ") << (waypoint_index + 1) << '/' << arraySize(waypoint) << "    ";
 }
 
 void setup() {
-  ButtonEvent.addButton(
-    buttonPin,
-    onDown,
-    onUp,
-    onHold,
-    5000,
-    onDouble,
-    200
-  );
+  button.clickHandler(onClick);
+  button.holdHandler(onHold, 5000);
 
   TimedEvent.addTimer(GET_FIX_TIMER, 5000, gpsFailure);
   TimedEvent.addTimer(READ_LOCATION_TIMER, 60000, initiateSleep);
@@ -339,6 +310,6 @@ void setup() {
 void loop() {
   while(Serial3.available()) gps << Serial3.read();
   TimedEvent.loop();
-  ButtonEvent.loop();
+  Button.process();
   stateMachine.update();
 }
